bebb88371b0c6e1d973c4a4d5c4e64c09b87b48c
leire-yagfer <leire.yagfer.1@educa.jcyl.es>
2024-10-14 10:46:25 +0200

    parte visual hecha

---
+++ AD_Entrega4_CRUDCoches_Mongo_ListTableView/src/main/java/org/example/ad_entrega4_crudcoches_mongo_listtableview/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.ad_entrega4_crudcoches_mongo_listtableview;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
