61ea53b79c4540806709ed0a0ac4618b9347e9ae
leire-yagfer <leire.yagfer.1@educa.jcyl.es>
2024-11-04 11:10:10 +0100

    controller y FX hecho

--- AD_Entrega7_CRUDCochesHibernate1N_JavaFX_CambioVistas/src/main/java/org/example/ad_entrega7_crudcocheshibernate1n_javafx_cambiovistas/Controller/MainController.java
+++ AD_Entrega7_CRUDCochesHibernate1N_JavaFX_CambioVistas/src/main/java/org/example/ad_entrega7_crudcocheshibernate1n_javafx_cambiovistas/Controller/MainController.java
@@ -1,14 +1,88 @@
 package org.example.ad_entrega7_crudcocheshibernate1n_javafx_cambiovistas.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button actualizarBoton;
+
+    @FXML
+    private Button borrarBoton;
+
+    @FXML
+    private TableColumn<?, ?> colMarca;
+
+    @FXML
+    private TableColumn<?, ?> colMatricula;
+
+    @FXML
+    private TableColumn<?, ?> colModelo;
+
+    @FXML
+    private TableColumn<?, ?> colTipo;
+
+    @FXML
+    private Button insertarBoton;
+
+    @FXML
+    private Button limpiarBoton;
+
+    @FXML
+    private TextField marcaTF;
+
+    @FXML
+    private TextField matriculaTF;
+
+    @FXML
+    private TextField modeloTF;
+
+    @FXML
+    private TableView<?> tableViewCoches;
+
+    @FXML
+    private ComboBox<?> tipoCB;
+
+    @FXML
+    private Button verMultasBoton;
+
+    @FXML
+    void onActualizarClick(ActionEvent event) {
+
     }
+
+    @FXML
+    void onBorrarClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onElegirCocheClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onInsertarClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLimpiarClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onVerMultasClick(ActionEvent event) {
+        //comprobar que hay un coche seleccionado
+        //cambiar de vista
+
+    }
+
 }
--- AD_Entrega7_CRUDCochesHibernate1N_JavaFX_CambioVistas/src/main/java/org/example/ad_entrega7_crudcocheshibernate1n_javafx_cambiovistas/Controller/MultasController.java
+++ AD_Entrega7_CRUDCochesHibernate1N_JavaFX_CambioVistas/src/main/java/org/example/ad_entrega7_crudcocheshibernate1n_javafx_cambiovistas/Controller/MultasController.java
@@ -1,4 +1,75 @@
 package org.example.ad_entrega7_crudcocheshibernate1n_javafx_cambiovistas.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class MultasController {
+
+    @FXML
+    private Button actualizarBoton;
+
+    @FXML
+    private Button borrarBoton;
+
+    @FXML
+    private TableColumn<?, ?> colFecha;
+
+    @FXML
+    private TableColumn<?, ?> colIdMulta;
+
+    @FXML
+    private TableColumn<?, ?> colPrecio;
+
+    @FXML
+    private DatePicker idFechaDatePicker;
+
+    @FXML
+    private TextField idMultaTF;
+
+    @FXML
+    private TextField idPrecioTF;
+
+    @FXML
+    private Button insertarBoton;
+
+    @FXML
+    private Button limpiarBoton;
+
+    @FXML
+    private TextField matriculaTF;
+
+    @FXML
+    private TableView<?> tableViewCoches;
+
+    @FXML
+    void onActualizarClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBorrarClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onElegirCocheClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onInsertarClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLimpiarClick(ActionEvent event) {
+
+    }
+
 }
