355e427d8160b4737c249fbaa5b3e1ce2ba934c9
leire-yagfer <leire.yagfer.1@educa.jcyl.es>
2025-01-27 09:28:06 +0100

    recién creado

---
+++ AD_Entrega9_ApiHotelJWT/src/main/java/com/example/ad_entrega9_apihoteljwt/AdEntrega9ApiHotelJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.ad_entrega9_apihoteljwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdEntrega9ApiHotelJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdEntrega9ApiHotelJwtApplication.class, args);
+	}
+
+}
---
+++ AD_Entrega9_ApiHotelJWT/src/test/java/com/example/ad_entrega9_apihoteljwt/AdEntrega9ApiHotelJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ad_entrega9_apihoteljwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdEntrega9ApiHotelJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
