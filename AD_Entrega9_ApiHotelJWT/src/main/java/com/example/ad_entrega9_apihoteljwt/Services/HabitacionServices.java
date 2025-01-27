cabdfb80dfa2eb2f2cd7200d8e4201fa4eadd32d
leire-yagfer <leire.yagfer.1@educa.jcyl.es>
2025-01-27 13:59:52 +0100

    services creadas

---
+++ AD_Entrega9_ApiHotelJWT/src/main/java/com/example/ad_entrega9_apihoteljwt/Services/HabitacionServices.java
@@ -0,0 +1,16 @@
+package com.example.ad_entrega9_apihoteljwt.Services;
+
+import com.example.ad_entrega9_apihoteljwt.Repositories.HabitacionRepositories;
+
+public class HabitacionServices {
+    private final HabitacionRepositories habitacionRepositories;
+
+
+    //CONSTRUCTOR CREADO PQ SI NO DA ERROR
+    public HabitacionServices(HabitacionRepositories habitacionRepositories) {
+        this.habitacionRepositories = habitacionRepositories;
+    }
+
+
+    //INDEXACIONES A REPOSITORIOS
+}
---
+++ AD_Entrega9_ApiHotelJWT/src/main/java/com/example/ad_entrega9_apihoteljwt/Services/HotelServices.java
@@ -0,0 +1,16 @@
+package com.example.ad_entrega9_apihoteljwt.Services;
+
+import com.example.ad_entrega9_apihoteljwt.Repositories.HotelRepositories;
+
+public class HotelServices {
+    private final HotelRepositories hotelRepositories;
+
+
+    //CONSTRUCTOR CREADO PQ SI NO DA ERROR
+    public HotelServices(HotelRepositories hotelRepositories) {
+        this.hotelRepositories = hotelRepositories;
+    }
+
+
+    //INDEXACIONES A REPOSITORIOS
+}
