fa2dfa93f0396c96685913aaff1f12b7f551a3a1
leire-yagfer <leire.yagfer.1@educa.jcyl.es>
2025-01-28 11:02:40 +0100

    repositories modificados con las especificaciones

--- AD_Entrega9_ApiHotelJWT/src/main/java/com/example/ad_entrega9_apihoteljwt/Repositories/HabitacionRepositories.java
+++ AD_Entrega9_ApiHotelJWT/src/main/java/com/example/ad_entrega9_apihoteljwt/Repositories/HabitacionRepositories.java
@@ -2,7 +2,18 @@ package com.example.ad_entrega9_apihoteljwt.Repositories;
 
 import com.example.ad_entrega9_apihoteljwt.Entities.Habitacion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface HabitacionRepositories extends JpaRepository<Habitacion, Integer> {
     //CONSULTAS
-}
+    //Búsqueda de habitaciones de un hotel por tamaño y precio (rango minimo→máximo). Solo mostrará aquellas habitaciones que estén marcadas como libres
+        @Query("SELECT h FROM Habitacion h WHERE h.hotel.id_hotel = :idHotel AND h.ocupada = false AND h.tamanio = :tamanio AND h.precio BETWEEN :precioMin AND :precioMax ORDER BY h.precio ASC")
+        List<Habitacion> findAvailableHabitaciones(int idhotel, float tamanio, double precioMin, double precioMax);
+        /*
+        1. h: Es un alias para la entidad Habitacion en la consulta JPQL.
+        2. h.hotel: Representa la relación entre la entidad Habitacion y la entidad Hotel. La entidad Habitacion tiene un campo hotel que está relacionado con la entidad Hotel mediante la anotación @ManyToOne. Esto indica que cada habitación pertenece a un hotel.
+        3. h.hotel.id_hotel: Dentro de la entidad Hotel, id_hotel es el identificador único del hotel. Esto hace referencia al campo id_hotel de la tabla Hotel en la base de datos.
+        */
+}//class
--- AD_Entrega9_ApiHotelJWT/src/main/java/com/example/ad_entrega9_apihoteljwt/Repositories/HotelRepositories.java
+++ AD_Entrega9_ApiHotelJWT/src/main/java/com/example/ad_entrega9_apihoteljwt/Repositories/HotelRepositories.java
@@ -3,6 +3,24 @@ package com.example.ad_entrega9_apihoteljwt.Repositories;
 import com.example.ad_entrega9_apihoteljwt.Entities.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HotelRepositories extends JpaRepository<Hotel, Integer> { //Integer pq la PK es Integer
     //CONSULTAS
+    //Búsqueda de hotel por localidad
+    List<Hotel> findHotelByLocalidad(String localidad);
+    //equivalente pero con consulta JPQL
+    /*
+    @Query("SELECT h FROM Hotel h WHERE h.localidad = :localidadParam")
+    List<Hotel> findHotelByLocalidad(@Param("localidadParam") String localidad);
+    List<Hotel> findHotelByLocalidad(String localidad); //equivalente de lo anterior sin @Param
+    */
+
+    //Búsqueda de hotel por categoría
+    List<Hotel> findHotelByCategoria(String categoria);
+    //equivalente pero con consulta JPQL
+    /*
+    @Query("SELECT h FROM Hotel h WHERE h.categoria = :categoria")
+    List<Hotel> findHotelByLocalidad(String categoria);
+    */
 }
